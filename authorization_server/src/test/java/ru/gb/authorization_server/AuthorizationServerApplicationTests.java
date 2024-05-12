23635c50332ccf826627149b3491e88bf67b8a40
вадим <funpalka1@gmail.com>
2024-05-12 18:00:12 +0400

    Oauth2

---
+++ authorization_server/src/test/java/ru/gb/authorization_server/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
